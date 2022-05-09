669e4994492d28aec15489a46321a6c11ca49069
Kleyson Cabral <kleyson.cabral3@gmail.com>
2022-05-09 19:17:54 +0000

    inicialização de sidebar e footer

---
+++ web/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export function MovieThumbnail() {
+    return (
+        <a href='#' className={styles.container}>
+            <p>Footer</p>
+        </a>
+    )
+}
---
+++ web/src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+export function MovieThumbnail() {
+    return (
+        <a href='#' className={styles.container}>
+           
+            <p>Sidebar</p>
+        </a>
+    )
+}
